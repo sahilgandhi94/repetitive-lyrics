9846f3692ddfbc7883c3f9ecc539d564bb27d399
Sahil Gandhi <sahilgandhi94@gmail.com>
2018-01-05 16:11:43 -0500

    requirements and finished billboards fetch

--- billboard-charts.py
+++ billboard-charts.py
@@ -1,21 +1,36 @@
-
+import logging
 from datetime import datetime, timedelta
 
 import billboard
-import logger
 
-SEPARATOR = ':-:'
-DATE_LIMIT = datetime.strptime('2017-01-01', '%Y-%m-%d')
+SEPARATOR = '<|=|>'
+DATE_LIMIT = datetime.strptime('1958-01-01', '%Y-%m-%d')
 CHART_NAME = 'billboard-200'
 
 CORPUS = dict()
-log = logger.getLogger('billboard-charts')
+
+# logging setup
+logger = logging.getLogger('billboard-charts')
+logger.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+fh = logging.FileHandler('logs.log')
+fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
 
 def getchart(name, date):
     ''' Fetches the given chart for the given date and stores in CORPUS '''
     str_date = str(date.date())
     chart = billboard.ChartData(name, date=str_date, fetch=True)
-    log.debug("Fetched {} songs".format(len(chart)))
+    logger.info("Fetched {} songs".format(len(chart)))
     for song in chart:
         CORPUS.update({song.title+SEPARATOR+song.artist: str_date})
 
@@ -29,9 +44,9 @@ def storecorpus():
 
 date = datetime.now()
 while date >= DATE_LIMIT:
-    log.info("Fetching data for: {}".format(date.date()))
+    logger.info("Fetching data for: {}".format(date.date()))
     getchart(CHART_NAME, date)
-    log.info("Size of corpus:    {}".format(len(CORPUS)))
+    logger.info("Size of corpus:    {}".format(len(CORPUS)))
     date = date - timedelta(days=7)
 storecorpus()
-log.info('Successfully created corpus.txt file')
+logger.info('Successfully created corpus.txt file')
