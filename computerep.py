3e3e42c634841a2cd873c3826215af82ac97cf57
Sahil Gandhi <sahilgandhi94@gmail.com>
2018-01-06 20:16:18 -0500

    rep computation

---
+++ computerep.py
@@ -0,0 +1,41 @@
+import sqlite3
+import traceback
+import zlib
+from datetime import datetime
+
+import constants
+import logger
+
+log = logger.getLogger('compute-rep')
+
+try:        
+    log.info('Connecting to db..')
+    conn = sqlite3.connect(constants.DATABASE)
+    c = conn.cursor()
+    log.info('Connected to db!')
+    exceptioncount = 0
+    repdump = list()
+    for row in c.execute('select * from lyrics'):
+        try:
+            songid, lyrics = row
+            ogbytes = lyrics.encode('utf-8')
+            compressedbytes = zlib.compress(ogbytes)
+            per = round((len(ogbytes)-len(compressedbytes))/len(ogbytes), 5)
+            repdump.append((songid, ogbytes, compressedbytes, per))
+        except Exception:
+            exceptioncount+=1
+            log.error('Rep compression exception \n{}'.format(traceback.format_exc()))
+            log.error('Rep Exception count: {}'.format(exceptioncount))
+            continue
+    
+    log.info('Dumping {} rep data to db..'.format(len(repdump)))
+    c.execute('insert into rep values (?,?,?,?)', repdump)
+    conn.commit()
+    log.info('Finished dumping rep data to db..')
+    
+except sqlite3.Error:
+    log.error('Sqlite connection error: {}'.format(traceback.format_exc()))
+except Exception:
+    log.error('Generic error: {}'.format(traceback.format_exc()))
+finally:
+    conn.close()
